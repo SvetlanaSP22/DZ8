feb107e1d7aed79e235cffeff430e1b6903e8d32
Svetlana <sypkovas@mail.ru>
2023-03-19 20:03:54 +0300

    решение 54 задачи

---
+++ task54/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// Например, задан массив:        В итоге получается вот такой массив:
+// 1 4 7 2                        7 4 2 1
+// 5 9 2 3                        9 5 3 2 
+// 8 4 2 4                        8 4 4 2
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-10, 11);
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(1); k++)
+                if (array[i, j] > array[i, k]) (array[i, j], array[i, k]) = (array[i, k], array[i, j]);
+
+}
+
+FillArray(array);
+PrintArray(array);
+SortArray(array);
+Console.WriteLine("Массив после сортировки:");
+PrintArray(array);
